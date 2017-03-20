486ff2d128ff92e54875ad95a19d4489f41201ee
Faradillah Nurul Hikmah <faradillah.nh@gmail.com>
2017-03-20 09:46:01 +0700

    Create ifelse.cpp
    File contoh program percabangan 2 kondisi menggunakan if else

---
+++ ifelse.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	int age = 12;
+
+	if (age>50)
+	{
+		cout<<"wow you are old";
+	} 
+	
+	if (age>50)
+	{
+		cout<<"wow you are old";
+	} else {
+		cout<<"wow you are under 50";
+	}
+
+	if (age>50)
+	{
+		if (age>100)
+		{
+			cout<<"Why are u still alive";
+		} else {
+			cout<<"you are old";
+		}
+	} else {
+		cout<<"wow you are under 50";
+	}
+	
+
+	return 0;
+}
